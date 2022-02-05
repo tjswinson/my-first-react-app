2981d3448bc9f13e8eb00250d120d181b1bef19f
tjswinson <tiffanyjswinson@gmail.com>
2022-02-05 12:50:32 -0500

    add counter

--- src/sectioning/main.jsx
+++ src/sectioning/main.jsx
@@ -1,20 +1,30 @@
-import Contact from "../components/contact"
+import { useState } from "react";
+import Contact from "../components/contact";
+import Counter from "../components/counter";
 import { mockResponse } from "../utils/mockResponse";
 
 const Main = () => {
+
+    
+
+    
 const response = mockResponse();
 const contacts = response.map((contact, index) =>{
 return (
-    <Contact contact={contact}  key={index} />
+    
+       <Contact contact={contact}  key={index} />
+    
     )
-    
-    
+        
     });
+
+    
 
 
     return (
         <>
             <main>
+            <Counter />
                 <ul>{contacts}</ul>
             </main>
         </>
