b0d2d45ab7d90f637f1ed6fc3224dd553dce2a29
tjswinson <tiffanyjswinson@gmail.com>
2022-02-12 12:00:17 -0500

    add event listener to single person on list

---
+++ src/components/contactSummary.jsx
@@ -0,0 +1,18 @@
+import Contact from "./contact";
+
+const ContactSummary = ({fields}) => {
+
+const handleClick = e => {
+    const target = e.target;
+    console.log(target);
+};
+
+    const contacts = fields.map((contact, index) => {
+        return <li key={index}>{contact.name}</li>;
+    });
+return (
+    <ul onClick={handleClick}>{contacts}</ul>
+);
+};
+
+export default ContactSummary;
--- src/sectioning/main.jsx
+++ src/sectioning/main.jsx
@@ -4,6 +4,7 @@ import ContactForm from "../components/contactForm";
 import { mockResponse } from "../utils/mockResponse";
 import Modal from '../components/modal';
 import {Route, Routes } from 'react-router-dom';
+import ContactSummary from "../components/contactSummary";
 
 const Main = () => {
   const [fields, setFields] = useState([]);
@@ -39,7 +40,7 @@ const contacts = fields.map((contact, index) => {
                 <Routes>
                 <Route path="/" element={<h1>Welcome to the Contact App</h1>} />
                     <Route path="add" element={<ContactForm action={handleSubmit}/>} />
-                    <Route path="list" element={<ul>{contacts}</ul>} />
+                    <Route path="list" element={<ContactSummary fields={fields} />} />
                     <Route path="*" element={<h1>Page Not Found</h1>} />
                 </Routes>
                 {isShowModal && <Modal action={handleIsShowModal} message="Contact added" />}
