69ff5cf57c23dc4de9ad6387b5332e9ae5017f65
tjswinson <tiffanyjswinson@gmail.com>
2022-02-15 21:17:37 -0500

    styling

--- src/sectioning/header.jsx
+++ src/sectioning/header.jsx
@@ -4,13 +4,11 @@ import { NavLink } from "react-router-dom";
 const Header = () => {
     return (
         <header>
-            <nav className="y-wrap">
-                <ul>
-                    <li>
-                        <NavLink to="/add">Add Contact</NavLink>
-                        
-                    </li>
-                    <li><NavLink to="/list">Contact List</NavLink></li>
+            <nav className="y-wrap navs navs--justify">
+                <div><NavLink to="/">Home</NavLink></div>
+                <ul className="navs navs--items">
+                    <li><NavLink to="/add">Add Contact</NavLink></li>
+                     <li><NavLink to="/list">Contact List</NavLink></li>
                 </ul>
             </nav>
         </header>
