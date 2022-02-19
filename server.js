343f62c56f0205989750060b9251ad282601bd5e
tjswinson <tiffanyjswinson@gmail.com>
2022-02-19 12:37:40 -0500

    render ui with react

--- server.js
+++ server.js
@@ -1,13 +1,20 @@
 require('dotenv').config();
 const express = require('express');
+const path = require('path');
 
 const server = express();
+
+server.use(express.static(path.resolve(__dirname + '/react-ui/build')));
 
 const { PORT } = process.env;
 server.get('/heartbeat', (req,res) => {
     res.json({
         "is":"working"
     });
+});
+
+server.get('*', (req, res)=> {
+    res.sendFile(path.resolve(__dirname, './react-ui/build', 'index.html'));
 });
 
 server.listen(PORT, () => {
