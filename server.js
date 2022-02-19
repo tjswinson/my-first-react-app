561896fab3acf528c40a5090f6f8093f023be89f
tjswinson <tiffanyjswinson@gmail.com>
2022-02-19 13:43:35 -0500

    address

--- server.js
+++ server.js
@@ -1,17 +1,39 @@
+//libraries
 require('dotenv').config();
 const express = require('express');
 const path = require('path');
+const Geocodio = require('geocodio-library-node');
 
+const { API_KEY, PORT } = process.env;
+
+//instantiations
+const geocoder = new Geocodio(API_KEY);
 const server = express();
 
+//middleware
 server.use(express.static(path.resolve(__dirname + '/react-ui/build')));
+server.use(express.json());
 
-const { PORT } = process.env;
+
 server.get('/heartbeat', (req,res) => {
     res.json({
         "is":"working"
     });
 });
+
+server.post('/location', (req, res) => {
+    const {address} = req.body;
+    
+  geocoder.geocode(address)
+  .then(response => {
+    res.json({response})
+  })
+  .catch(err => {
+    res.json({err})
+  }
+);
+})
+
 
 server.get('*', (req, res)=> {
     res.sendFile(path.resolve(__dirname, './react-ui/build', 'index.html'));
