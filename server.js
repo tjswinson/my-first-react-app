10b03b116c057df1820b821e27b17dbb2c4721fc
tjswinson <tiffanyjswinson@gmail.com>
2022-02-19 11:52:41 -0500

    hide values in dotenv

--- server.js
+++ server.js
@@ -1,14 +1,16 @@
+require('dotenv').config();
 const express = require('express');
 
 const server = express();
 
+const { PORT } = process.env;
 server.get('/heartbeat', (req,res) => {
     res.json({
         "is":"working"
     });
 });
 
-server.listen('8080', () => {
-    console.log('server is listening at port 8080');
+server.listen(PORT, () => {
+    console.log(`server is listening at port ${PORT}`);
 
 });
